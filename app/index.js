e7e36a2f16d8d2a1e0edae711598fcb8610a61d7
King Wilder <king@gizmobeach.com>
2019-01-04 14:24:31 -0800

    Refactored the index file to a server.js file.

--- app/index.js
+++ app/index.js
@@ -1,83 +1,24 @@
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
-var greetingRoutes = require('./lib/routes/greeting.routes');
-var userRoutes = require('./lib/routes/user.routes');
-var tokenRoutes = require('./lib/routes/token.routes');
-var checksRoutes = require('./lib/routes/checks.routes');
+// Dependencies
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
-var httpServer = http.createServer(function(req, res) {
-    unifiedServer(req, res);
-});
+// Declare the app
+var app = {};
 
-httpServer.listen(config.httpPort, function() {
-    console.log(`Http server listening on port ${config.httpPort}.`);
-});
+// Init function
+app.init = function(){
 
-var httpServerOptions = {
-    'key': fs.readFileSync('./ssl/server.key'),
-    'cert': fs.readFileSync('./ssl/server.crt')
-};
-var httpsServer = https.createServer(httpServerOptions, function(req, res) {
-    unifiedServer(req, res);
-});
+  // Start the server
+  server.init();
 
-httpsServer.listen(config.httpsPort, function() {
-    console.log(`Https server listening on port ${config.httpsPort}.`);
-});
+  // Start the workers
+  workers.init();
 
-
-var unifiedServer = function(req, res) {
-    var parsedUrl = url.parse(req.url, true);
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    var method = req.method.toLowerCase();
-    var queryString = parsedUrl.query;
-    var headers = req.headers;
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function(data) {
-        buffer += decoder.write(data);
-    });
-    req.on('end', function() {
-        buffer += decoder.end();
-        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-        var data = {
-            'path': trimmedPath,
-            'method': method,
-            'queryString': queryString,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        };
-
-        chosenHandler(data, function(statusCode, payload) {
-            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
-            payload = typeof(payload) === 'object' ? payload : {};
-
-            var payloadString = JSON.stringify(payload);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-            console.log('Returning response: ', statusCode, payloadString);
-        });
-    }); 
 };
 
-/**
- * Extracted routes to individual files for ease of maintenance.
- */
-var router = {
-    'ping': handlers.ping,
-    'hello': greetingRoutes.hello,
-    'users': userRoutes.users,
-    'token': tokenRoutes.tokens,
-    'checks': checksRoutes.checks
-};
+// Self executing
+app.init();
 
+
+// Export the app
+module.exports = app;
---
+++ app/lib/server.js
@@ -0,0 +1,94 @@
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
+var fs = require('fs');
+var handlers = require('./handlers');
+var helpers = require('./helpers');
+var greetingRoutes = require('./routes/greeting.routes');
+var userRoutes = require('./routes/user.routes');
+var tokenRoutes = require('./routes/token.routes');
+var checksRoutes = require('./routes/checks.routes');
+var path = require('path');
+
+var server = {};
+
+server.httpServer = http.createServer(function(req, res) {
+    server.unifiedServer(req, res);
+});
+
+
+
+server.httpServerOptions = {
+    'key': fs.readFileSync(path.join(__dirname, '../ssl/server.key')),
+    'cert': fs.readFileSync(path.join(__dirname, '../ssl/server.crt'))
+};
+server.httpsServer = https.createServer(server.httpServerOptions, function(req, res) {
+    server.unifiedServer(req, res);
+});
+
+
+
+
+server.unifiedServer = function(req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    var method = req.method.toLowerCase();
+    var queryString = parsedUrl.query;
+    var headers = req.headers;
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+        var chosenHandler = typeof(server.router[trimmedPath]) !== 'undefined' ? server.router[trimmedPath] : handlers.notFound;
+
+        var data = {
+            'path': trimmedPath,
+            'method': method,
+            'queryString': queryString,
+            'headers': headers,
+            'payload': helpers.parseJsonToObject(buffer)
+        };
+
+        chosenHandler(data, function(statusCode, payload) {
+            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+            payload = typeof(payload) === 'object' ? payload : {};
+
+            var payloadString = JSON.stringify(payload);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log('Returning response: ', statusCode, payloadString);
+        });
+    }); 
+};
+
+/**
+ * Extracted routes to individual files for ease of maintenance.
+ */
+server.router = {
+    'ping': handlers.ping,
+    'hello': greetingRoutes.hello,
+    'users': userRoutes.users,
+    'token': tokenRoutes.tokens,
+    'checks': checksRoutes.checks
+};
+
+server.init = function() {
+    server.httpServer.listen(config.httpPort, function() {
+        console.log(`Http server listening on port ${config.httpPort}.`);
+    });
+
+    server.httpsServer.listen(config.httpsPort, function() {
+        console.log(`Https server listening on port ${config.httpsPort}.`);
+    });
+};
+
+
+module.exports = server;
